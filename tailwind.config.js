931c024d4cc535ab25e9ef602fd1f75d34310b29
usman-j <ujamshaid@averyberkel.com>
2025-05-07 16:37:49 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./views/**/*.ejs", "./src/**/*.css"],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1f2937',
+          accent: '#e11d48'
+        }
+      }
+    },
+    plugins: []
+};
